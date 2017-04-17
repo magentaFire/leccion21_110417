119fe14550959873ed2e2effc94beaacc66fe3ba
magentaFire <dafne.magenta@gmail.com>
2017-04-17 12:24:57 -0500

    Hecha animacion de imagen 1 sin boton de cierre

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+var taymanModal = document.getElementById("taymanModal");
+var img = document.getElementById("taymanImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+img.addEventListener("click", function(){
+  taymanModal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+});
