d0422754f75b28a2561b749ed4de30b032aec68f
magentaFire <dafne.magenta@gmail.com>
2017-04-17 12:28:53 -0500

    añadido botón de cierre funcional

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,13 @@ var img = document.getElementById("taymanImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 
-img.addEventListener("click", function(){
+img.addEventListener ("click", function(){
   taymanModal.style.display = "block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
 });
+
+var span = document.getElementsByClassName("close")[0];
+span.addEventListener ("click", function() {
+  taymanModal.style.display = "none";
+}); 
